6835b779728fe4471366691419b1b4e76fc2ca2c
sld666666 <sld666666@gmail>
2013-10-25 19:35:23 +0800

    add ui project

---
+++ common/ui/frame/FrameWindowImplBase.h
@@ -0,0 +1,31 @@
+#ifndef COMMON_UI_FRAMEWNDCLASSINFO_H
+#define COMMON_UI_FRAMEWNDCLASSINFO_H
+
+class  FrameWinClassInfo
+{
+	ATOM register(HINSTANCE hInstance)
+	{
+		WNDCLASSEX wcex;
+
+		wcex.cbSize = sizeof(WNDCLASSEX);
+
+		wcex.style			= CS_HREDRAW | CS_VREDRAW;
+		wcex.lpfnWndProc	= WndProc;
+		wcex.cbClsExtra		= 0;
+		wcex.cbWndExtra		= 0;
+		wcex.hInstance		= hInstance;
+		wcex.hIcon			= LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINAPPTESTOR));
+		wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+		wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+		wcex.lpszMenuName	= MAKEINTRESOURCE(IDC_WINAPPTESTOR);
+		wcex.lpszClassName	= szWindowClass;
+		wcex.hIconSm		= LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+		return RegisterClassEx(&wcex);
+	}
+
+};
+
+
+#endif // COMMON_UI_FRAMEWNDCLASSINFO_H
+
