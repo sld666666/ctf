16c05cf8e4ce6cc82bfbeace5491b97f12971868
sld666666 <sld666666@gmail.com>
2013-10-30 22:35:56 +0800

    update demo.cpp

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -4,18 +4,16 @@
 #include "stdafx.h"
 #include "demo.h"
 #include "frame/FrameWndClassInfo.h"
-#include "core/MessageLoop.h"
 #include "frame/FrameWindowImpl.h"
 
 
-
 // 全局变量:
-HINSTANCE hInst;								// 当前实例
+
 
 // 此代码模块中包含的函数的前向声明:
 BOOL				InitInstance(HINSTANCE, int);
 
-INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+//INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
@@ -26,111 +24,32 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
 
 	FrameWinClassInfo().MyRegisterClass(hInstance);
 
-	if (!InitInstance (hInstance, nCmdShow))
+	ui::FrameWindowImpl frame;
+	if (!frame.create(hInstance, nCmdShow))
 	{
 		return FALSE;
 	}
-	return core::MessageLoop().run(hInstance);
+	return frame.exec();
 }
 
 
-
-
-
-//
-//   函数: InitInstance(HINSTANCE, int)
-//
-//   目的: 保存实例句柄并创建主窗口
-//
-//   注释:
-//
-//        在此函数中，我们在全局变量中保存实例句柄并
-//        创建和显示主程序窗口。
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
-{
-   HWND hWnd;
-
-   hInst = hInstance; // 将实例句柄存储在全局变量中
-
-   hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
-}
-
-//
-//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  目的: 处理主窗口的消息。
-//
-//  WM_COMMAND	- 处理应用程序菜单
-//  WM_PAINT	- 绘制主窗口
-//  WM_DESTROY	- 发送退出消息并返回
-//
-//
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	int wmId, wmEvent;
-	PAINTSTRUCT ps;
-	HDC hdc;
-
-	switch (message)
-	{
-	case WM_COMMAND:
-		wmId    = LOWORD(wParam);
-		wmEvent = HIWORD(wParam);
-		// 分析菜单选择:
-		switch (wmId)
-		{
-		case IDM_ABOUT:
-			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-			break;
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-		break;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		// TODO: 在此添加任意绘图代码...
-		EndPaint(hWnd, &ps);
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
-}
 
 // “关于”框的消息处理程序。
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	UNREFERENCED_PARAMETER(lParam);
-	switch (message)
-	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
-			return (INT_PTR)TRUE;
-		}
-		break;
-	}
-	return (INT_PTR)FALSE;
-}
+// INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+// {
+// 	UNREFERENCED_PARAMETER(lParam);
+// 	switch (message)
+// 	{
+// 	case WM_INITDIALOG:
+// 		return (INT_PTR)TRUE;
+// 
+// 	case WM_COMMAND:
+// 		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+// 		{
+// 			EndDialog(hDlg, LOWORD(wParam));
+// 			return (INT_PTR)TRUE;
+// 		}
+// 		break;
+// 	}
+// 	return (INT_PTR)FALSE;
+// }
